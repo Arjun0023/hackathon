f8b1aee68b4d464776f8d893001c0fb7a24e09ab
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-02-17 15:50:16 +0530

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import Chart from 'chart.js/auto';
 
 const CloudPlatformCalculator = () => {
   const [computeHours, setComputeHours] = useState(0);
